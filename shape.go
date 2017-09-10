a12855a327d7076014a5aa9dfca2a2d11df3cceb
ledao <zhaowei153@126.com>
2017-09-10 12:16:28 +0800

    为shape.go文件中函数添加单元测试用例

--- shape.go
+++ shape.go
@@ -24,13 +24,17 @@ func (a *Arrf) Reshape(shape ...int) *Arrf {
 	return a
 }
 
+//两个多维数组形状相同，则返回true， 否则返回false。
 func (a *Arrf) SameShapeTo(b *Arrf) bool {
 	return SameIntSlice(a.shape, b.shape)
 }
 
+//将多个两维数组在垂直方向上组合起来，形成新的多维数组。
+//不影响原多维数组。
 func Vstack(arrs ...*Arrf) *Arrf {
 	for i := range arrs {
 		if arrs[i].Ndims() > 2 {
+			fmt.Println("in Vstack function, array dimension cannot bigger than 2.")
 			panic(SHAPE_ERROR)
 		}
 	}
@@ -41,40 +45,44 @@ func Vstack(arrs ...*Arrf) *Arrf {
 		return arrs[0].Copy()
 	}
 
-	var vlenSum int = 0
-
-	var hlen int
-	if arrs[0].Ndims() == 1 {
-		hlen = arrs[0].shape[0]
-		vlenSum += 1
-	} else {
-		hlen = arrs[0].shape[1]
-		vlenSum += arrs[0].shape[0]
-	}
-	for i := 1; i < len(arrs); i++ {
-		var nextHen int
-		if arrs[i].Ndims() == 1 {
-			nextHen = arrs[i].shape[0]
-			vlenSum += 1
-		} else {
-			nextHen = arrs[i].shape[1]
-			vlenSum += arrs[i].shape[0]
-		}
-		if hlen != nextHen {
-			panic(SHAPE_ERROR)
-		}
-	}
-
-	data := make([]float64, vlenSum*hlen)
-	var offset = 0
-	for i := range arrs {
-		copy(data[offset:], arrs[i].data)
-		offset += len(arrs[i].data)
-	}
-
-	return Array(data, vlenSum, hlen)
+	return Concat(0, arrs...)
+	//
+	//var vlenSum int = 0
+	//
+	//var hlen int
+	//if arrs[0].Ndims() == 1 {
+	//	hlen = arrs[0].shape[0]
+	//	vlenSum += 1
+	//} else {
+	//	hlen = arrs[0].shape[1]
+	//	vlenSum += arrs[0].shape[0]
+	//}
+	//for i := 1; i < len(arrs); i++ {
+	//	var nextHen int
+	//	if arrs[i].Ndims() == 1 {
+	//		nextHen = arrs[i].shape[0]
+	//		vlenSum += 1
+	//	} else {
+	//		nextHen = arrs[i].shape[1]
+	//		vlenSum += arrs[i].shape[0]
+	//	}
+	//	if hlen != nextHen {
+	//		panic(SHAPE_ERROR)
+	//	}
+	//}
+	//
+	//data := make([]float64, vlenSum*hlen)
+	//var offset = 0
+	//for i := range arrs {
+	//	copy(data[offset:], arrs[i].data)
+	//	offset += len(arrs[i].data)
+	//}
+	//
+	//return Array(data, vlenSum, hlen)
 }
 
+//将多个两维数组在水平方向上组合起来，形成新的多维数组。
+//不影响原多维数组。
 func Hstack(arrs ...*Arrf) *Arrf {
 	for i := range arrs {
 		if arrs[i].Ndims() > 2 {
@@ -88,52 +96,65 @@ func Hstack(arrs ...*Arrf) *Arrf {
 		return arrs[0].Copy()
 	}
 
-	var hlenSum int = 0
-	var hBlockLens = make([]int, len(arrs))
-	var vlen int
-	if arrs[0].Ndims() == 1 {
-		vlen = 1
-		hlenSum += arrs[0].shape[0]
-		hBlockLens[0] = arrs[0].shape[0]
-	} else {
-		vlen = arrs[0].shape[0]
-		hlenSum += arrs[0].shape[1]
-		hBlockLens[0] = arrs[0].shape[1]
-	}
-	for i := 1; i < len(arrs); i++ {
-		var nextVlen int
-		if arrs[i].Ndims() == 1 {
-			nextVlen = 1
-			hlenSum += arrs[i].shape[0]
-			hBlockLens[i] = arrs[i].shape[0]
-		} else {
-			nextVlen = arrs[i].shape[0]
-			hlenSum += arrs[i].shape[1]
-			hBlockLens[i] = arrs[i].shape[1]
-		}
-		if vlen != nextVlen {
-			panic(SHAPE_ERROR)
-		}
-	}
+	return Concat(1, arrs...)
 
-	data := make([]float64, hlenSum*vlen)
-	for i := 0; i < vlen; i++ {
-		var curPos = 0
-		for j := 0; j < len(arrs); j++ {
-			copy(data[curPos+i*hlenSum:curPos+i*hlenSum+hBlockLens[j]], arrs[j].data[i*hBlockLens[j]:(i+1)*hBlockLens[j]])
-			curPos += hBlockLens[j]
-		}
-	}
-
-	return Array(data, vlen, hlenSum)
+	//var hlenSum int = 0
+	//var hBlockLens = make([]int, len(arrs))
+	//var vlen int
+	//if arrs[0].Ndims() == 1 {
+	//	vlen = 1
+	//	hlenSum += arrs[0].shape[0]
+	//	hBlockLens[0] = arrs[0].shape[0]
+	//} else {
+	//	vlen = arrs[0].shape[0]
+	//	hlenSum += arrs[0].shape[1]
+	//	hBlockLens[0] = arrs[0].shape[1]
+	//}
+	//for i := 1; i < len(arrs); i++ {
+	//	var nextVlen int
+	//	if arrs[i].Ndims() == 1 {
+	//		nextVlen = 1
+	//		hlenSum += arrs[i].shape[0]
+	//		hBlockLens[i] = arrs[i].shape[0]
+	//	} else {
+	//		nextVlen = arrs[i].shape[0]
+	//		hlenSum += arrs[i].shape[1]
+	//		hBlockLens[i] = arrs[i].shape[1]
+	//	}
+	//	if vlen != nextVlen {
+	//		panic(SHAPE_ERROR)
+	//	}
+	//}
+	//
+	//data := make([]float64, hlenSum*vlen)
+	//for i := 0; i < vlen; i++ {
+	//	var curPos = 0
+	//	for j := 0; j < len(arrs); j++ {
+	//		copy(data[curPos+i*hlenSum:curPos+i*hlenSum+hBlockLens[j]], arrs[j].data[i*hBlockLens[j]:(i+1)*hBlockLens[j]])
+	//		curPos += hBlockLens[j]
+	//	}
+	//}
+	//
+	//return Array(data, vlen, hlenSum)
 }
 
+//将多个多维数组在指定的轴上组合起来。
+//一维数组默认扩充为2维，参考AtLeast2D函数。
 func Concat(axis int, arrs ...*Arrf) *Arrf {
 	if len(arrs) == 0 {
 		return nil
 	}
 	if len(arrs) == 1 {
 		return arrs[0].Copy()
+	}
+
+	for i := range arrs {
+		AtLeast2D(arrs[i])
+	}
+
+	if axis >= arrs[0].Ndims() {
+		fmt.Println("axis is bigger than dimensions num.")
+		panic(PARAMETER_ERROR)
 	}
 
 	var newShape = make([]int, arrs[0].Ndims())
@@ -174,6 +195,7 @@ func Concat(axis int, arrs ...*Arrf) *Arrf {
 	return Array(data, newShape...)
 }
 
+//将一维数组扩充位二维
 func AtLeast2D(a *Arrf) *Arrf {
 	if a == nil {
 		return nil
@@ -188,7 +210,7 @@ func AtLeast2D(a *Arrf) *Arrf {
 	}
 }
 
-//
+//将数组内部的元素铺平返回，创建新的数据副本。
 func (a *Arrf) Flatten() *Arrf {
 	ra := make([]float64, len(a.data))
 	copy(ra, a.data)
--- shape_test.go
+++ shape_test.go
@@ -17,17 +17,181 @@ func TestArrf_Reshape(t *testing.T) {
 	}
 }
 
-func TestHstack(t *testing.T) {
-	var a = Arange(10)
-	var b = Arange(10)
-	t.Log(Hstack(a, b, a, b))
+func TestArrf_ReshapeException(t *testing.T) {
+	defer func() {
+		r := recover()
+		if r != SHAPE_ERROR {
+			t.Errorf("Expected shape error, got ", r)
+		}
+	}()
+
+	Arange(4).Reshape(5)
 }
 
+func TestArrf_SameShapeTo(t *testing.T) {
+	a := Arange(4).Reshape(2,2)
+	b := Array([]float64{3,4,5,6}, 2, 2)
+	if a.SameShapeTo(b) != true {
+		t.Errorf("Expected true, got ", a.SameShapeTo(b))
+	}
+}
+
+func TestVstack(t *testing.T) {
+	if Vstack() != nil {
+		t.Errorf("Expected nil, got ", Vstack())
+	}
+
+	a := Arange(3)
+	stacked := Vstack(a)
+	if !stacked.Equal(Arange(3)).AllTrues() {
+		t.Errorf("Expected [0, 1, 2], got ", stacked)
+	}
+
+	b := Array([]float64{3,4,5})
+	stacked = Vstack(a, b)
+	if !stacked.Equal(Array([]float64{0,1,2,3,4,5}, 2, 3)).AllTrues(){
+		t.Errorf("Expected [[0 1 2] [3 4 5]], got ", stacked)
+	}
+
+	a = Arange(2)
+	b = Arange(4).Reshape(2,2)
+	stacked = Vstack(a, b)
+	if !stacked.Equal(Array([]float64{0,1,0,1,2,3}, 3,2)).AllTrues() {
+		t.Errorf("Expected [[0,1], [0,1], [2, 3]], got ", stacked)
+	}
+}
+
+func TestVstackException(t *testing.T) {
+	a := Arange(4).Reshape(1,2,2)
+	defer func() {
+		r := recover()
+		if r != SHAPE_ERROR {
+			t.Errorf("Expected shape error, got ", r)
+		}
+	}()
+
+	Vstack(a)
+}
+
+func TestVstackException2(t *testing.T) {
+	a := Arange(4)
+	b := Arange(5)
+	defer func() {
+		r := recover()
+		if r != SHAPE_ERROR {
+			t.Errorf("Expected shape error, got ", r)
+		}
+	}()
+
+	Vstack(a, b)
+}
+
+func TestHstack(t *testing.T) {
+	if Hstack() != nil {
+		t.Errorf("Expected nil, got ", Hstack())
+	}
+
+	a := Arange(3)
+	stacked := Hstack(a)
+	if !stacked.Equal(Arange(3)).AllTrues() {
+		t.Errorf("Expected [0, 1, 2], got ", stacked)
+	}
+	a = a.Reshape(3, 1)
+	b := Array([]float64{3,4,5}).Reshape(3, 1)
+	stacked = Hstack(a, b)
+	if !stacked.Equal(Array([]float64{0,3,1,4,2,5}, 3, 2)).AllTrues(){
+		t.Errorf("Expected [[0 3] [1 4], [2 5]], got ", stacked)
+	}
+
+	a = Arange(2).Reshape(2, 1)
+	b = Arange(4).Reshape(2,2)
+	stacked = Hstack(a, b)
+	if !stacked.Equal(Array([]float64{0, 0, 1, 1, 2, 3}, 2, 3)).AllTrues() {
+		t.Errorf("Expected [[0, 0, 1], [1, 2, 3]], got ", stacked)
+	}
+}
+
+func TestHstackException(t *testing.T) {
+	a := Arange(4).Reshape(1,2,2)
+	defer func() {
+		r := recover()
+		if r != SHAPE_ERROR {
+			t.Errorf("Expected shape error, got ", r)
+		}
+	}()
+
+	Hstack(a)
+}
+
+func TestHstackException2(t *testing.T) {
+	a := Arange(4).Reshape(4, 1)
+	b := Arange(5).Reshape(5, 1)
+	defer func() {
+		r := recover()
+		if r != SHAPE_ERROR {
+			t.Errorf("Expected shape error, got ", r)
+		}
+	}()
+
+	Hstack(a, b)
+}
+
+
+
 func TestConcat(t *testing.T) {
-	var a = Arange(1, 11).Reshape(2, 5)
-	var b = Arange(10).Reshape(2, 5)
-	t.Log(Concat(0, a, b))
-	t.Log(Concat(1, a, b))
+	if Concat(0) != nil {
+		t.Errorf("Expected nil, got ", Concat(0))
+	}
+	concated := Concat(0, Arange(2))
+	if !concated.Equal(Arange(2)).AllTrues() {
+		t.Errorf("Expected [0, 1], got ", concated)
+	}
+
+	a := Arange(3)
+	b := Arange(1, 4)
+
+	concated = Concat(0, a, b)
+	if !concated.Equal(Array([]float64{0,1,2, 1,2,3}, 2, 3)).AllTrues() {
+		t.Errorf("Expected [[0,1,2], [1,2,3]], got ", concated)
+	}
+
+	a = Arange(3)
+	b = Arange(1, 4)
+
+	concated = Concat(1, a, b)
+	t.Log(concated)
+	if !concated.Equal(Array([]float64{0,1,2,1,2,3}, 1,6)).AllTrues() {
+		t.Errorf("Expected [[0,1,2,1,2,3]], got ", concated)
+	}
+
+}
+
+func TestConcatException(t *testing.T) {
+	a := Arange(4)
+	b := Arange(1, 4)
+
+	defer func() {
+		r := recover()
+		if r != SHAPE_ERROR {
+			t.Errorf("Expected shape error, got ", r)
+		}
+	}()
+
+	Concat(0, a, b)
+}
+
+func TestConcatException2(t *testing.T) {
+	a := Arange(4)
+	b := Arange(1, 4)
+
+	defer func() {
+		r := recover()
+		if r != PARAMETER_ERROR {
+			t.Errorf("Expected PARAMETER_ERROR, got ", r)
+		}
+	}()
+
+	Concat(2, a, b)
 }
 
 func TestAtLeast2D(t *testing.T) {
@@ -45,5 +209,34 @@ func TestAtLeast2D(t *testing.T) {
 
 	if AtLeast2D(nil) != nil {
 		t.Error("Expected nil, got ", AtLeast2D(nil))
+	}
+}
+
+func TestAtLeast2D2(t *testing.T) {
+	if AtLeast2D(nil) != nil {
+		t.Errorf("Expected nil, got ", AtLeast2D(nil))
+	}
+
+	arr := Arange(3)
+	AtLeast2D(arr)
+
+	if !SameIntSlice(arr.shape, []int{1, 3}) {
+		t.Errorf("expected true, got false")
+	}
+
+	arr = Arange(3).Reshape(3, 1)
+	AtLeast2D(arr)
+
+	if !SameIntSlice(arr.shape, []int{3, 1}) {
+		t.Errorf("expected true, got false")
+	}
+}
+
+func TestArrf_Flatten(t *testing.T) {
+	arr := Arange(3).Reshape(3, 1)
+	flattened := arr.Flatten()
+
+	if !flattened.SameShapeTo(Arange(3)) {
+		t.Errorf("expected [3], got ", flattened.shape)
 	}
 }
