da6fcd1b840d9d2baad7e9b466d8b43fd770a25c
davezhao <zhaowei153@126.com>
2018-09-20 10:01:39 +0800

    add demo c code

--- blas/cblas_level1.go
+++ blas/cblas_level1.go
@@ -9,10 +9,26 @@ package blas
 #include <stdio.h>
 #include "cblas.h"
 
+//demo
+int printArray(double* p, int len) {
+	printf("%d, %f\n", len, p[2]);
+}
+
 */
 import "C"
 
-func Go_cblas_ddot(n int64, x []float64, incx int64, y []float64, incy int64) float64 {
+import (
+	"unsafe"
+)
 
+//一维Float64 Slice转换为C的一维数组，返回数组头指针和数组长度。
+//共享内存地址。
+func SliceToCArrayFloat64(slice []float64) (*C.double, C.int) {
+	return (*C.double)(unsafe.Pointer(&slice[0])), C.int(len(slice))
+}
+
+func Go_cblas_ddot(n int64, x []float64, incx int64, y []float64, incy int64) float64 {
+	cx, lx := SliceToCArrayFloat64(x)
+	C.printArray(cx, lx)
 	return 0.0
 }
--- blas/cblas_level1_test.go
+++ blas/cblas_level1_test.go
@@ -3,7 +3,7 @@ package blas
 import "testing"
 
 func Test_cblas_ddot(t *testing.T) {
-	res := Go_cblas_ddot(1, []float64{}, 1, []float64{}, 1)
+	res := Go_cblas_ddot(1, []float64{1, 2, 3}, 1, []float64{2}, 1)
 	if 0.0 != res {
 		t.Error("expected 0.0, got ", res)
 	}
