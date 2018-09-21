e4fb33028fd666dc8b1fb835a2fdc5d76b0f3db9
davezhao <zhaowei153@126.com>
2018-09-21 09:23:48 +0800

    add clbas_daxpy func

--- blas/cblas_level_one.go
+++ blas/cblas_level_one.go
@@ -9,11 +9,6 @@ package blas
 #include <stdio.h>
 #include "cblas.h"
 
-//demo
-int printArray(double* p, int len) {
-	printf("%d, %f\n", len, p[2]);
-}
-
 */
 import "C"
 
@@ -23,4 +18,12 @@ func Go_cblas_ddot(n int64, x []float64, incx int64, y []float64, incy int64) fl
 	cx, lx := SliceToCArrayFloat64(x)
 	cy, _ := SliceToCArrayFloat64(y)
 	return float64(C.cblas_ddot(lx, cx, C.int(incx), cy, C.int(incy)))
+}
+
+//y := a*x + y
+//ref: https://software.intel.com/en-us/mkl-developer-reference-c-cblas-axpy#C8CBB256-EAB7-4629-80FF-14029038E6B7
+func Go_cblas_daxpy(n int64, a float64, x []float64, incx int64, y []float64, incy int64) {
+	cx, _ := SliceToCArrayFloat64(x)
+	cy, _ := SliceToCArrayFloat64(y)
+	C.cblas_daxpy(C.int(n), C.double(a), cx, C.int(incx), cy, C.int(incy))
 }
